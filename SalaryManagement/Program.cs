e37d2b80cff90ac9d62d5d3f180ae5355654ff29
DuyDat <leyeunghe@gmail.com>
2024-05-08 15:37:36 +0700

    edit hello

--- SalaryManagement/Program.cs
+++ SalaryManagement/Program.cs
@@ -14,6 +14,7 @@ namespace SalaryManagement
         [STAThread]
         static void Main()
         {
+            //hello anhdat
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
